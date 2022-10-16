cb4b2334cfadc0a8a06dd9133c1ab74c5ff89796
Leo <leoyuan0816@berkeley.edu>
2022-10-16 00:57:57 -0700

    web基本界面实现2

--- web/schedule.js
+++ web/schedule.js
@@ -1,34 +1,19 @@
-
 let courseList = [];
 let semester;
-let year;
-
-
-function buttonClicked(valueClicked) {
-    if (isNaN(parseInt(valueClicked))) { //NaN means "Not a Number"
-        //Hint: call a function we just created!
-        makesSymbol(valueClicked);
-    } else {
-        //Hint: call a function we just created!
-        makesNumber(valueClicked);
-    }
-    document.querySelector(".result-screen").innerText = strbuffer;
-    // Hint: we need to change what number appears on the screen! to change html, one listener you could use is querySelector
-}
 
 function setListeners() {
-    // document.querySelector(".buttons").addEventListener("click", function(event) {
-    //     buttonClicked(event.target.innerText);
-    // });
-    //Hint: We want to select all buttons from html and make it so that something happens when you click on the buttons! querySelectorAll might be helpful
     let addButton = document.querySelector("#addCourse");
     addButton.addEventListener("click", function () {
-        let inputVal = document.getElementById("inputCourse").value;
-        if (!courseList.includes(inputVal) && inputVal !== "") {
-            courseList.push(inputVal)
-            document.getElementById("inputCourse").value = "";
+        let inputSub = document.getElementById("inputSubject").value;
+        let inputNum = document.getElementById("inputNumber").value;
+        if (inputSub === "" || inputNum === "") {
+            document.querySelector("#courses").innerText = "Lack of course Subject or Number!"
+        } else if (!courseList.includes(inputSub + "-" + inputNum) && inputSub !== "") {
+            courseList.push(inputSub + "-" + inputNum);
             document.querySelector("#courses").innerText = "Current Courses: " + courseList;
         }
+        document.getElementById("inputSubject").value = "";
+        document.getElementById("inputNumber").value = "";
     })
 
     let deleteButton = document.querySelector("#deleteCourse");
@@ -37,7 +22,6 @@ function setListeners() {
         document.querySelector("#courses").innerText = "Current Courses: " + courseList;
     })
 
-    // let id = $("#semester option:checked").val();
     let semesterButton = document.querySelector("#semesterSubmit");
     semesterButton.addEventListener("click", function () {
         let id = document.querySelector("select").value;
@@ -45,9 +29,6 @@ function setListeners() {
         semester = "name";
         document.querySelector("#semester1").innerText = "Semester Selected: " + name;
     })
-    //Hint: addEventListener might be useful.
-    //Hint: event.target.innerText might be helpful. innerText return type is a string
 }
 
-//Make sure to call setListeners!!!
 setListeners();
