1eeb36981d818ea53e8b8044d04e01f174c2d4ee
Kirtoo <kirtoo@MSI.localdomain>
2022-10-16 05:03:39 -0700

    setting up main

---
+++ main.py
@@ -0,0 +1,6 @@
+semester = input("please input semester(E.g. Fall-2022): ")
+course = input("please input course name(E.g. compsci 61A)")
+course_subject = course.split()[0]
+course_index = course.split()[1]
+print(course_subject)
+print(course_index)
